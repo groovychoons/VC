40ab8ce52b302e3c91f3851c17708f2140f4b3e9
Zara Siddique <zara@siddique.eu>
2018-04-26 01:11:42 +0100

    landing page complete bar latest requests

--- public/styles.bundle.js
+++ public/styles.bundle.js
@@ -64,7 +64,7 @@ exports.push([module.i, "@import url(https://fonts.googleapis.com/css?family=Ope
 exports.push([module.i, "@import url(https://fonts.googleapis.com/css?family=Raleway);", ""]);
 
 // module
-exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\n\n* { font-family: Raleway; }\n \n\nh1 { font-family: Open Sans !important; }\nh2 { font-family: Open Sans !important; }\nh3 { font-family: Open Sans !important; }\nh4 { font-family: Open Sans !important; }\nh5 { font-family: Open Sans !important; }\nh6 { font-family: Open Sans !important; }", ""]);
+exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\n\n* { font-family: Raleway; }\n \n\nh1 { font-family: Open Sans !important; }\nh2 { font-family: Open Sans !important; }\nh3 { font-family: Open Sans !important; }\nh4 { font-family: Open Sans !important; }\nh5 { font-family: Open Sans !important; }\nh6 { font-family: Open Sans !important; }\n\nbody {\n\theight: 100vh;\n}\n\napp-root {\n  display: block;\n  height: 100vh;\n}\n", ""]);
 
 // exports
 
